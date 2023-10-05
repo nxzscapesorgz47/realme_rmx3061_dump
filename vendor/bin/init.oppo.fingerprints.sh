9061accf0c8596b9a8d935a12bc55cf6b77c7e8e
nxzscapesorgz47 <nxzhuuu47@gmail.com>
2023-10-05 16:58:13 +0000

    Add vendor for full_oppo6765-user 10 QP1A.190711.020 bedd37e98646d3a1 release-keys
    Signed-off-by: nxzscapesorgz47 <nxzhuuu47@gmail.com>

---
+++ vendor/bin/init.oppo.fingerprints.sh
@@ -0,0 +1,16 @@
+#!/system/bin/sh
+
+chown system system /sys/bus/platform/devices/soc/soc\:fpc_interrupt/clk_enable
+chown system system /sys/bus/platform/devices/soc/soc\:fpc_interrupt/wakelock_enable
+chown system system /sys/bus/platform/devices/soc/soc\:fpc_interrupt/irq
+chown system system /sys/bus/platform/devices/soc/soc\:fpc_interrupt/irq_enable
+chown system system /sys/bus/platform/devices/soc/soc\:fpc_interrupt/regulator_disable
+chmod 0200 /sys/bus/platform/devices/soc/soc\:fpc_interrupt/irq_enable
+chmod 0200 /sys/bus/platform/devices/soc/soc\:fpc_interrupt/clk_enable
+chmod 0200 /sys/bus/platform/devices/soc/soc\:fpc_interrupt/wakelock_enable
+chmod 0600 /sys/bus/platform/devices/soc/soc\:fpc_interrupt/irq
+chmod 0200 /sys/bus/platform/devices/soc/soc\:fpc_interrupt/regulator_disable
+
+#add for silead
+chown system:system /dev/silead_fp
+chmod 0666 /dev/silead_fp
